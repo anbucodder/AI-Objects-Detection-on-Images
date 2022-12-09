2b572f26ac7654cfb1fc30f4cce8eb5d453717e4
anbucodder <100592337+anbucodder@users.noreply.github.com>
2022-12-09 19:05:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function bed(){
+    window.location = "bedroom.html";
+}
+function tvac(){
+    window.location = "tv-ac.html";
+}
+function fruit(){
+    window.location = "fruitbasket.html";
+}
+function desk(){
+    window.location = "desk.html";
+}
